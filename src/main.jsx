a62d474c27a414e3551ccf3ec8fac1e44be889c5
Gaëtan Jestin <gaetanjestin@outlook.com>
2023-05-05 22:52:45 +0200

    added scss and UI component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './index.scss'
+import './reset.css'
+import Overlay from './overlay'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<App />
+		<Overlay />
+	</React.StrictMode>
 )
---
+++ src/overlay.jsx
@@ -0,0 +1,11 @@
+export default function Overlay() {
+	return <Intro />
+}
+
+function Intro() {
+	return (
+		<div className='intro-container'>
+			<h1>React Three Fiber</h1>
+		</div>
+	)
+}
