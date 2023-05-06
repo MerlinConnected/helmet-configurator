3b234fa6a2181fa5b8e498b673c9642c27276399
Gaëtan Jestin <gaetanjestin@outlook.com>
2023-05-06 17:02:59 +0200

    Added individual maps for outer-shell textures

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,106 @@
 import { useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls, useGLTF, Center, Environment, AccumulativeShadows, RandomizedLight, MeshTransmissionMaterial } from '@react-three/drei'
+import {
+	OrbitControls,
+	useGLTF,
+	Center,
+	Environment,
+	AccumulativeShadows,
+	RandomizedLight,
+	MeshTransmissionMaterial,
+	useTexture,
+	Decal,
+} from '@react-three/drei'
 import { easing } from 'maath'
 
 export function Model(props) {
 	const { nodes, materials } = useGLTF('/BellHelmet.glb')
+	const albedo = useTexture('helmet-outer-shell-albedo-blue.jpg')
+	const normal = useTexture('helmet-outer-shell-normal.jpg')
+	const metalness = useTexture('helmet-outer-shell-metalness.jpg')
+	const roughness = useTexture('helmet-outer-shell-roughness.jpg')
+
 	return (
 		<group {...props} dispose={null}>
 			<mesh castShadow receiveShadow geometry={nodes.Visor.geometry}>
 				<MeshTransmissionMaterial transmissionSampler />
 			</mesh>
-			<mesh castShadow receiveShadow geometry={nodes.Outer_Shell.geometry} material={materials['Outer Shell']} />
-			<mesh castShadow receiveShadow geometry={nodes.model002.geometry} material={materials.Stinching} />
-			<mesh castShadow receiveShadow geometry={nodes.model002_1.geometry} material={materials['Inner Shell']} />
-			<mesh castShadow receiveShadow geometry={nodes.model002_2.geometry} material={materials.Plastics} />
-			<mesh castShadow receiveShadow geometry={nodes.model002_3.geometry} material={materials['Black Metal']} />
-			<mesh castShadow receiveShadow geometry={nodes.model002_4.geometry} material={materials.Spring} />
-			<mesh castShadow receiveShadow geometry={nodes.model002_5.geometry} material={materials.Screw} />
-			<mesh castShadow receiveShadow geometry={nodes.model002_6.geometry} material={materials.Rubber} />
-			<mesh castShadow receiveShadow geometry={nodes.model002_7.geometry} material={materials['Inside Fabric']} />
-			<mesh castShadow receiveShadow geometry={nodes.model002_8.geometry} material={materials['Outside Fabric']} />
-			<mesh castShadow receiveShadow geometry={nodes.model002_9.geometry} material={materials.Inlays} />
-			<mesh castShadow receiveShadow geometry={nodes.model002_10.geometry} material={materials['Shell Plastics']} />
+			<mesh castShadow receiveShadow geometry={nodes.Outer_Shell.geometry}>
+				<meshPhysicalMaterial
+					map={albedo}
+					normalMap={normal}
+					metalnessMap={metalness}
+					roughnessMap={roughness}
+					clearcoat={1}
+				/>
+			</mesh>
+
+			<mesh
+				castShadow
+				receiveShadow
+				geometry={nodes.model002.geometry}
+				material={materials.Stinching}
+			/>
+			<mesh
+				castShadow
+				receiveShadow
+				geometry={nodes.model002_1.geometry}
+				material={materials['Inner Shell']}
+			/>
+			<mesh
+				castShadow
+				receiveShadow
+				geometry={nodes.model002_2.geometry}
+				material={materials.Plastics}
+			/>
+			<mesh
+				castShadow
+				receiveShadow
+				geometry={nodes.model002_3.geometry}
+				material={materials['Black Metal']}
+			/>
+			<mesh
+				castShadow
+				receiveShadow
+				geometry={nodes.model002_4.geometry}
+				material={materials.Spring}
+			/>
+			<mesh
+				castShadow
+				receiveShadow
+				geometry={nodes.model002_5.geometry}
+				material={materials.Screw}
+			/>
+			<mesh
+				castShadow
+				receiveShadow
+				geometry={nodes.model002_6.geometry}
+				material={materials.Rubber}
+			/>
+			<mesh
+				castShadow
+				receiveShadow
+				geometry={nodes.model002_7.geometry}
+				material={materials['Inside Fabric']}
+			/>
+			<mesh
+				castShadow
+				receiveShadow
+				geometry={nodes.model002_8.geometry}
+				material={materials['Outside Fabric']}
+			/>
+			<mesh
+				castShadow
+				receiveShadow
+				geometry={nodes.model002_9.geometry}
+				material={materials.Inlays}
+			/>
+			<mesh
+				castShadow
+				receiveShadow
+				geometry={nodes.model002_10.geometry}
+				material={materials['Shell Plastics']}
+			/>
 		</group>
 	)
 }
@@ -30,15 +109,32 @@ useGLTF.preload('/BellHelmet.glb')
 
 function Backdrop() {
 	return (
-		<AccumulativeShadows temporal frames={60} alphaTest={0.85} scale={10} position={[0, -0.22, 0]}>
-			<RandomizedLight amount={12} radius={20} intensity={0.9} ambient={5} position={[5, 5, -10]} />
+		<AccumulativeShadows
+			temporal
+			frames={60}
+			alphaTest={0.85}
+			scale={10}
+			position={[0, -0.22, 0]}
+		>
+			<RandomizedLight
+				amount={12}
+				radius={20}
+				intensity={0.9}
+				ambient={5}
+				position={[5, 5, -10]}
+			/>
 		</AccumulativeShadows>
 	)
 }
 
 export default function App() {
 	return (
-		<Canvas eventSource={document.getElementById('root')} eventPrefix='client' shadows camera={{ fov: 60, position: [0, 0, 2] }}>
+		<Canvas
+			eventSource={document.getElementById('root')}
+			eventPrefix='client'
+			shadows
+			camera={{ fov: 60, position: [0, 0, 2] }}
+		>
 			<CamRig>
 				<Center>
 					<Model />
@@ -47,7 +143,7 @@ export default function App() {
 			</CamRig>
 			<ambientLight intensity={0.5} />
 			<Environment preset='studio' />
-			{/* <OrbitControls /> */}
+			<OrbitControls />
 		</Canvas>
 	)
 }
@@ -56,7 +152,12 @@ function CamRig({ children }) {
 	const group = useRef()
 
 	useFrame((state, delta) => {
-		easing.dampE(group.current.rotation, [-state.pointer.y / 10, state.pointer.x / 5, 0], 0.25, delta)
+		easing.dampE(
+			group.current.rotation,
+			[-state.pointer.y / 30, state.pointer.x / 40, 0],
+			0.25,
+			delta
+		)
 	})
 
 	return <group ref={group}>{children}</group>
--- src/overlay.jsx
+++ src/overlay.jsx
@@ -5,7 +5,16 @@ export default function Overlay() {
 function Intro() {
 	return (
 		<div className='intro-container'>
-			<h1>React Three Fiber</h1>
+			<h1>Bell Helmet</h1>
+			<button>Configure !</button>
+		</div>
+	)
+}
+
+function Customizer() {
+	return (
+		<div className='customizer-container'>
+			<h1>Customizer</h1>
 		</div>
 	)
 }
